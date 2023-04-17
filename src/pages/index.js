782b70d7d73e0495703da130bda4211b02339c52
mn-react-js-dev <mayur.nandwalker.5057@gmail.com>
2023-04-17 18:14:37 +0530

    chenges

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import HomePage from './homePage';
-const inter = Inter({ subsets: ['latin'] })
+ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
