86db5752bb81eecf5fd330142ff0b9c64c6e9dc6
mn-react-js-dev <mayur.nandwalker.5057@gmail.com>
2023-04-17 17:42:02 +0530

    employee speajk page done

---
+++ src/pages/employeeSpeaks.js
@@ -0,0 +1,51 @@
+import Header from '@/commonComponent/Header'
+import React from 'react'
+import EmployeespeakImage from '../assets/images/employeespeakimage.png'
+import Image from 'next/image'
+import ProfileImage from '../assets/images/profile.png'
+
+
+const employeeSpeaks = () => {
+    return (
+        <>
+            <Header />
+            <section className='employee-speak' style={{ "background": "#E3E3FF" }}>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-sm-6'>
+                            <div className='mt-5'>
+
+                                <h1>Employee Speak</h1>
+                                <p>
+                                    We are a company that offers design
+                                    and develop services for you from initial
+                                    sketches to the final construction.
+                                </p>
+                            </div>
+                        </div>
+                        <div className='col-sm-6'>
+                            <Image src={EmployeespeakImage} class="img-fluid" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='qualities-we-take'>
+                <div className='container'>
+                    <div className='row mb-5 mt-5 cst-border-hjohn pt-5 pb-5'>
+                        <div className='col-md-2 col-4' >
+                            <Image src={ProfileImage}/>
+                        </div>
+                        <div className='col-md-10 col-8'>
+                            <h2>John doe</h2>
+                            <p>Creative Design Head </p>
+                            <p>Total Experience: 15 Years</p>
+                            <p>A team with years of development, design and marketing experience who understand the complexity and needs of your business.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default employeeSpeaks
