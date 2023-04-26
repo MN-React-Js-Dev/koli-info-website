b13e97894646811b6896136fa7b5d9b3c1f228fa
swapnil <swapnil.patil@koliinfotech.com>
2023-04-26 10:47:47 +0530

    bootstrap script added

--- src/PagesComponent/ContactUs/ContactUsScreen.js
+++ src/PagesComponent/ContactUs/ContactUsScreen.js
@@ -7,16 +7,57 @@ const ContactUsScreen = () => {
     <>
       <section>
         <div className="container">
-          <div className="row text-center m-5">
-            <div className="d-flex">
-              <div className="col-5 p-3">
+          <div className="row  m-3 d-flex">
+            {/* <div className=""> */}
+              <div className="col-lg-5 p-3">
                 <h1 className=" pt-5 mt-5 bold-font">Share Your App URL</h1>
-                <div className="cst-hr-for-process " />
+                <div className="cst-hr-for-process" />
+                <div className=" m-2 mb-3">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Your Name"
+                    name="Your Name"
+                  />
+                </div>
+                <div className=" m-2 mb-3">
+                  <input
+                    type="email"
+                    class="form-control"
+                    placeholder="Email"
+                    name="Email"
+                  />
+                </div>
+                <div className=" m-2 mb-3">
+                  <input
+                    type="Number"
+                    class="form-control"
+                    placeholder="Phone Number"
+                    name="Phone Number"
+                  />
+                </div>
+                <a class="btn contact-us-btn m-3" type="submit">
+                  Send Now
+                </a>
+                <div className="m-3 mb-4">
+                  <p className="boldP">USA :</p>
+                  <p>
+                    We are a company that offers design and develop services for
+                    you from initial sketches to the final construction.
+                  </p>
+                </div>
+                <div className="m-3">
+                  <p  className="boldP">India :</p>
+                  <p>
+                    We are a company that offers design and develop services for
+                    you from initial sketches to the final construction.
+                  </p>
+                </div>
               </div>
-              <div className="col-7 p-3">
+              <div className="col-lg-7 p-3">
                 <Image src={mapImage} className="img-fluid" />
               </div>
-            </div>
+            {/* </div> */}
           </div>
         </div>
       </section>
--- src/PagesComponent/ContactUs/HeroSection.js
+++ src/PagesComponent/ContactUs/HeroSection.js
@@ -11,7 +11,7 @@ const ContactUsHeroSection = () => {
       >
         <div className="container">
           <div className="row" style={{ alignItems: "center" }}>
-            <div className="col-lg-6 mt-5 text-start">
+            <div className="col-lg-6 text-start">
               <h1 className="hero-sec-header">Contact</h1>
               <p className="para-for-hero">
                 We are a company that offers design and develop services for you
