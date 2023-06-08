0060f82c0f7be778e1cbd13f680cb44a09dc5e00
KOLI-36 <sweta.pandey.5057@gmail.com>
2023-06-08 11:43:27 +0530

    first commit

--- src/PagesComponent/HomePage/ScheduleAnAppointment.js
+++ src/PagesComponent/HomePage/ScheduleAnAppointment.js
@@ -14,7 +14,19 @@ const ScheduleAnAppointment = () => {
       <section className="mt-5 mt-5" style={{ background: "#3F689F" }}>
         <div className="container">
           <div className="row">
-            <div className="col-sm-6"> </div>
+            <div className="col-sm-6">
+              <div className="mt-5 mb-2">
+                <div className="ratio ratio-1x1">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7442.266399503546!2d72.87774862484953!3d21.147096793737376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be051862e39c1e3%3A0xf0b3f8e98ab74dc4!2sKOLI%20Infotech%20Pvt.%20Ltd.!5e0!3m2!1sen!2sin!4v1682938315739!5m2!1sen!2sin"
+                    style={{ border: 0 }}
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                  ></iframe>
+                </div>
+              </div>
+            </div>
             <div className="col-sm-6">
               <div className="mt-5 mb-2">
                 <h2 className="text-white mb-3 schedule-appointment">
