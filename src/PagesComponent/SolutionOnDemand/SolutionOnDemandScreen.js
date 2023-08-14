0d20a6131ccd1590b18ebf59f78b6614c8b219a7
Jagdish Pawar <jagadish.pawar.5057@gmail.com>
2023-08-14 10:59:44 +0530

    make some changes in solution on demand page images & logos

--- src/PagesComponent/SolutionOnDemand/SolutionOnDemandScreen.js
+++ src/PagesComponent/SolutionOnDemand/SolutionOnDemandScreen.js
@@ -4,12 +4,7 @@ import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import SODimage1 from "../../assets/images/SODimage1.png";
 import SODimage2 from "../../assets/images/SODimage2.png";
 import SODimage3 from "../../assets/images/SODimage3.png";
-import bookindMdl from "../../assets/images/bookindMdl.png";
 import onDemCrsl1 from "../../assets/images/onDemCrsl-1.png";
-import onDemCrsl2 from "../../assets/images/onDemCrsl-2.png";
-import onDemCrsl3 from "../../assets/images/onDemCrsl-3.png";
-import LeftArrow from '.././../assets/images/leftArrow.png'
-import RightArrow from '.././../assets/images/rightArrow.png'
 // --------------------------------------------------------------------
 import AppDevelopment from '.././../assets/images/mobile-app-development-trends.png'
 // import AppDevelopment from '.././../assets/images/App-development.jpg'
@@ -17,6 +12,22 @@ import mobileApp from '.././../assets/images/vaival-application.png'
 import AppTesting from '.././../assets/images/mobile-app-testing.jpg'
 import AppSecuriry from '.././../assets/images/mobile-application-security.jpg'
 import AppTestingQa from '.././../assets/images/mobile-app-development-testing-QA.png'
+import BookingModule from '../../assets/images/BookingModule.png'
+import InAppPayments from '../../assets/images/InAppPayments.png'
+import OrderingonTheGo from '../../assets/images/OrderingonTheGo.png'
+import SchedulingabookingOrder from '../../assets/images/SchedulingabookingOrder.png'
+import OrderTracking from '../../assets/images/OrderTracking.png'
+import LiveChat from '../../assets/images/LiveChat.png'
+// - ------------------------ out top project on demand -----------------------------------
+import AmericanExpress from '../../assets/images/AmericanExpress.png'
+import ByjusLogo1 from '../../assets/images/ByjusLogo1.png'
+import HDFC from '../../assets/images/HDFC.png'
+import Axis from '../../assets/images/Axis.png'
+import Onfleek from "../../assets/images/onfleek.png";
+import SetFlow from '../../assets/images/SetFlow.png'
+import jodiBanao  from '../../assets/images/jodiBanao.png'
+
+
 import { motion } from "framer-motion";
 
 import { COMMAN_TEXT } from "@/commonComponent/commanText";
@@ -27,31 +38,37 @@ import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from "react-ico
 const SolutionOnDemandScreen = () => {
   const data = [
     {
+      image: BookingModule,
       service: "Booking Module",
       detail:
         "Our booking module provides a seamless and user-friendly platform for customers to easily book services or appointments.",
     },
     {
+      image: InAppPayments,
       service: "In-App Payments",
       detail:
         "Our in-app payments feature enables secure and convenient transactions directly within the app, providing a seamless user experience.",
     },
     {
+      image: OrderingonTheGo,
       service: "Ordering on-The-Go",
       detail:
         "Our ordering on-the-go feature allows users to conveniently place orders and make purchases directly from their mobile devices, anytime and anywhere.",
     },
     {
+      image: SchedulingabookingOrder,
       service: "Scheduling a booking/ order",
       detail:
         "Our scheduling feature enables users to easily schedule/book appointments or orders based on their preferred date, time, and availability.",
     },
     {
+      image: OrderTracking,
       service: "Order Tracking",
       detail:
         "Our order tracking system allows users to track the progress and status of their orders in real-time, providing transparency and peace of mind.",
     },
     {
+      image: LiveChat,
       service: "Real-Time calls and Messages",
       detail:
         "Our real-time communication feature enables users to make instant voice and video calls, as well as exchange messages in real-time, fostering seamless and efficient communication.",
@@ -132,8 +149,7 @@ const SolutionOnDemandScreen = () => {
             <div className="text-center">
               <a
                 href="#"
-                className="btn btn-danger m-4 "
-
+                className="btn btn-danger m-4"
                 type="submit"
               >
                 Get Your Free Consultation
@@ -146,52 +162,52 @@ const SolutionOnDemandScreen = () => {
         <div className="container">
           <div className="" data-aos="fade-up">
             <h1 className="text-center pt-5 comman-heading">
-              Top On-Demand Mobile Apps & Solutions Developed by Us
+              Top On-Demand Solutions Developed by Us
             </h1>
             <div className="cst-hr-for-process mb-5"></div>
             <div className="row blog text-center">
               <div className="img-slide-box" alt="Image not found" ref={carousel}>
                 <div className="item">
-                  <div className="image">
+                  <div className="logos-image">
                     <Image
-                      src={AppDevelopment}
+                      src={AmericanExpress}
                       alt="image"
-                      className="slide-img m-2"
+                      className="slide-img m-2 p-2 WebDev-dashed-border"
                     />
                     <Image
-                      src={mobileApp}
+                      src={ByjusLogo1}
                       alt="image"
-                      className="slide-img m-2"
+                      className="slide-img m-2 p-2 WebDev-dashed-border"
                     />
                     <Image
-                      src={AppTesting}
+                      src={HDFC}
                       alt="image"
-                      className="slide-img m-2"
+                      className="slide-img m-2 p-2 WebDev-dashed-border"
                     />
                     <Image
-                      src={AppSecuriry}
+                      src={Axis}
                       alt="image"
-                      className="slide-img m-2"
+                      className="slide-img m-2 p-2 WebDev-dashed-border"
                     />
                     <Image
-                      src={AppTestingQa}
+                      src={Onfleek}
                       alt="image"
-                      className="slide-img m-2"
+                      className="slide-img m-2 p-2 WebDev-dashed-border"
                     />
                     <Image
-                      src={AppDevelopment}
+                      src={SetFlow}
                       alt="image"
-                      className="slide-img m-2"
+                      className="slide-img m-2 p-2 WebDev-dashed-border"
                     />
                     <Image
-                      src={onDemCrsl1}
+                      src={jodiBanao}
                       alt="image"
-                      className="slide-img m-2"
+                      className="slide-img m-2  p-2 WebDev-dashed-border"
                     />
                   </div>
                 </div>
               </div>
-            {/*   <div className="buttons m-3">
+              {/*   <div className="buttons m-3">
                 <button onClick={handleLeftClick} className="m-4 p-1">
     
                   <Image style={{ borderRadius: '50%', height: '40px', width: '40px' }} src={LeftArrow}></Image>
@@ -200,30 +216,30 @@ const SolutionOnDemandScreen = () => {
                   <Image style={{ borderRadius: '50%', height: '40px', width: '40px' }} src={RightArrow}></Image>
                 </button>
               </div> */}
-                <div className="d-flex justify-content-center">
-            <motion.div
-              className="box"
-              whileHover={{ scale: 1.1 }}
-              transition={{
-                type: "spring",
-                stiffness: 400,
-                damping: 10,
-              }}
-            >
-              <span className="px-3 arrowIcon" ><BsFillArrowLeftCircleFill style={{ color: "black" }} onClick={handleLeftClick} /></span>
-            </motion.div>
-            <motion.div
-              className="box"
-              whileHover={{ scale: 1.1 }}
-              transition={{
-                type: "spring",
-                stiffness: 400,
-                damping: 10,
-              }}
-            >
-             <span className="px-3 arrowIcon"><BsFillArrowRightCircleFill style={{ color: "black" }} onClick={handleRightClick} /></span>
-            </motion.div>
-          </div>
+              <div className="d-flex justify-content-center">
+                <motion.div
+                  className="box"
+                  whileHover={{ scale: 1.1 }}
+                  transition={{
+                    type: "spring",
+                    stiffness: 400,
+                    damping: 10,
+                  }}
+                >
+                  <span className="px-3 arrowIcon" ><BsFillArrowLeftCircleFill style={{ color: "black" }} onClick={handleLeftClick} /></span>
+                </motion.div>
+                <motion.div
+                  className="box"
+                  whileHover={{ scale: 1.1 }}
+                  transition={{
+                    type: "spring",
+                    stiffness: 400,
+                    damping: 10,
+                  }}
+                >
+                  <span className="px-3 arrowIcon"><BsFillArrowRightCircleFill style={{ color: "black" }} onClick={handleRightClick} /></span>
+                </motion.div>
+              </div>
             </div>
           </div>
         </div>
@@ -235,14 +251,14 @@ const SolutionOnDemandScreen = () => {
           </h1>
           <div className="cst-hr-for-process mb-5" />
           <div className="row" data-aos="fade-up" >
-            {data.map(({ service, detail }, index) => (
+            {data.map(({ image, service, detail }, index) => (
               <>
                 <div className="col-sm-4 mb-4 d-flex" key={index}>
-                  <div className="p-5 bold-p WebDev-dashed-border text-center ">
+                  <div className="p-5 bold-p WebDev-dashed-border text-center">
                     <Image
-                      src={bookindMdl}
+                      src={image}
                       alt="Image not found"
-                      className="mb-4 mt-4 img-fluid "
+                      className="mb-4 mt-4 solutionImage"
                     />
                     <p>{service}</p>
                     <span>{detail}</span>
@@ -258,3 +274,4 @@ const SolutionOnDemandScreen = () => {
 };
 
 export default SolutionOnDemandScreen;
+  
